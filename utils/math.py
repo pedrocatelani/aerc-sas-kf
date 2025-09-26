77d6fe88d982db1dd8391b9a955283af1e61d299
pedrocatelani <pedrocatelani@gmail.com>
2025-09-26 13:40:20 -0300

    feat(math): Objective function

--- utils/math.py
+++ utils/math.py
@@ -6,6 +6,73 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 
+# Função para gerar um arquivo para análise de dados.
+def generate_predictions():
+    tests = []
+    for deck, value in results.items():
+        random_test = {
+            "atr": get_attributes_list(value[0]),
+            "expected": value[1],
+            "prediction": 0,
+        }
+
+        new_x = np.array([random_test["atr"]])
+        new_x_poly = poly.transform(new_x)
+
+        prediction = model.predict(new_x_poly)
+
+        print(
+            "Valor previsto: {}, Valor esperado: {}, Indivíduo: {}".format(
+                prediction, random_test["expected"], deck
+            )
+        )
+        random_test["prediction"] = float(prediction[0])
+
+        tests.append(random_test)
+
+    with open("cross-prediction.json", "w") as payload:
+        json.dump(tests, payload)
+
+# Função objetivo
+def objective_function(coef: list, term: float, atr: object):
+    ae = atr["expectedAember"]
+    ac = atr["aemberControl"]
+    cc = atr["creatureControl"]
+    cp = atr["creatureProtection"]
+    ep = atr["effectivePower"]
+    dr = atr["disruption"]
+
+    result = (
+        (ae * coef[0])
+        + (ac * coef[1])
+        + (cc * coef[2])
+        + (cp * coef[3])
+        + (ep * coef[4])
+        + (dr * coef[5])
+        + ((ae**2) * coef[6])
+        + ((ae * ac) * coef[7])
+        + ((ae * cc) * coef[8])
+        + ((ae * cp) * coef[9])
+        + ((ae * ep) * coef[10])
+        + ((ae * dr) * coef[11])
+        + ((ac**2) * coef[12])
+        + ((ac * cc) * coef[13])
+        + ((ac * cp) * coef[14])
+        + ((ac * ep) * coef[15])
+        + ((ac * dr) * coef[16])
+        + ((cc**2) * coef[17])
+        + ((cc * cp) * coef[18])
+        + ((cc * ep) * coef[19])
+        + ((cc * dr) * coef[20])
+        + ((cp**2) * coef[21])
+        + ((cp * ep) * coef[22])
+        + ((cp * dr) * coef[23])
+        + ((ep**2) * coef[24])
+        + ((ep * dr) * coef[25])
+        + ((dr**2) * coef[26])
+    ) + term
+
+    return result
 
 # Função utilitária (é uma arma secreta que utilizaremos no futuro!)
 def get_attributes_list(deck: object):
@@ -63,6 +130,7 @@ print(f"R-squared: {score}")
 # A função F(x) é representada por esses coeficientes
 print("Coeficientes (c_n):", model.coef_)
 print("Termo de intercepção (b):", model.intercept_)
+print(poly.get_feature_names_out(input_features=["ae", "ac", "cc", "cp", "ep", "dr"]))
 
 
 # Exemplo de como usar a função para prever um novo valor
@@ -85,3 +153,5 @@ for t in tests:
             prediction, t["expected"], t["deck"]
         )
     )
+
+generate_predictions()
